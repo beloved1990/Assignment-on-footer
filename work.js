905607232bdd681344f3f09038c58fab89023196
beloved <moss.heaven@gmail.com>
2025-07-18 13:38:12 +0100

    Add files via upload

---
+++ work.js
@@ -0,0 +1,92 @@
+/** addition operator sign 
+let a = 15;
+let b = 20;
+ let  sum = a + b;
+ console.log("The sum of " + a + " and " + b + " is: " + sum);
+
+ //subtraction operator sign
+ let c = 25;
+ let d = 50;
+ let difference = d - c;
+    console.log("The difference between  d   and   c   is: ",difference);
+
+    //another method of subtraction
+let e = 25;
+ let f = 50;
+ let differences= e - f;
+    console.log("The differences between  e   and   f   is: ",differences);
+
+    // multiplication operator
+    let product = a * b;
+    console.log("The product of a  and  b  is: ", product);
+
+    
+    let products = e * f;
+    console.log("The products of e  and  f  is: ", products);
+
+//division operator
+    division = b / a;
+    console.log("The division of b by a is: ", division);
+
+    //another way of division
+
+    let quotient = e/f;
+    console.log("the quotient of e and f is:" , quotient);
+
+    //modulus operator
+
+    let remainder = f % e;
+    console.log("The remainder of f divided by e is: ", remainder);
+
+    //another way of modulus operator
+    let remainders = c % d;
+    console.log("The remainders of c divided by d is: ", remainders);
+
+    // practices on increment prefix and postfix operators 
+    // prefix increment operator
+    let x = 5;  
+    let y = 10;
+    x +=3;
+    console.log("The value of x after prefix increment is: ", x);
+
+    y +=70;
+    console.log("The value of y after prefix increment is: ", y);
+
+    //subtract prefix operator
+    let z = 20;
+    let w = 30;
+    z -= 4.5;
+    console.log("The value of z after prefix decrement is: ", z);
+
+    w -= 12.5;
+    console.log("The value of w after prefix decrement is: ", w);
+
+    //multiplication prefix operator
+    z *= 4;
+    console.log("The value of z after prefix multiplication is: ", z);
+    w *= 2;
+    console.log("The value of w after prefix multiplication is: ", w);
+    */
+
+    //Assignment 2 on modulus for odd number
+    let a = 10;
+    let b = 3;
+    let remainder = a % b;
+    console.log("the ramainder of a divided by b is: ", remainder);
+
+    //modulus for even number
+    w = 12;
+    z = 4;
+    let remainders = w % z;
+    console.log("the remainder of w divided by z is:", remainder);
+
+    w %= 4; // a = a % 2
+console.log("Value of w after %= 4: ", w); // 0
+
+    
+    z %= 3; 
+console.log("Value of z after %= 3: ", z); // 1
+
+
+   
+  
